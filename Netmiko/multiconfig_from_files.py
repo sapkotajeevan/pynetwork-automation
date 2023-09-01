c14fccbccb729b1cee4435e81ca41257446507f4
Jeevan Sapkota <sapkotajeevan100@gmail.com>
2023-09-01 17:26:21 +0545

    IP in 'devices.txt', push router configs from file

---
+++ Netmiko/multiconfig_from_files.py
@@ -0,0 +1,34 @@
+from netmiko import ConnectHandler
+
+with open('devices.txt','r') as f:
+    devices = f.read().splitlines()
+
+device_list = list()
+
+for ip in devices:
+    cisco_device = {
+                    'device_type':'cisco_ios',
+                    'ip':ip,
+                    'username':'jeevan',
+                    'password':'cisco',
+                    'secret':'cisco',
+                    'port':22,
+                    'verbose':True
+    }
+    device_list.append(cisco_device)
+
+print(device_list)
+
+for device in device_list:
+    print('Connecting to ' + device['ip'])
+    connection = ConnectHandler(**device)
+
+    print('Entering enable mode...')
+    connection.enable()
+
+    file = input('Enter configuration file (use a valid path) for ' + device['ip'] + ' : ')
+    print('Running commands from file:',file,'to device:',device['ip'])
+    output =connection.send_config_from_file(file)
+    print(output)
+
+    connection.disconnect()
